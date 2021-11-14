cbb8c0903cc31844c9b2564c6bd24356dfaa1537
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-11-13 21:13:02 -0800

    screencap ML

--- ML/video.py
+++ ML/video.py
@@ -9,12 +9,11 @@ import logging
 import requests
 
 def main():
+    os.environ['DISPLAY'] = ':0'
     url="http://localhost:5000/emotion"
     isPosition=sys.argv[1]
     if len(sys.argv) >2:
         url=sys.argv[2]
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
-    logging.getLogger('tensorflow').setLevel(logging.FATAL)
     emotions={
         'neutral':0,
         'angry':1,
@@ -37,29 +36,33 @@ def main():
     maxVal=0
     old_stdout = sys.stdout
 
-    bounding_box = {'left': 160, 'top': 160, 'width': 200, 'height': 200}
-    sct = mss()
-    while True:
-        sct_img = sct.grab(bounding_box)
-        frame=np.array(sct_img)
+    bounding_box = {'left': 160, 'top': 160, 'width': 200, 'height': 200,"mon":0}
 
-        if (isPosition != "true"):
-            #sys.stdout = open(os.devnull, "w")
-            result=DeepFace.analyze(frame,enforce_detection=False,models=('emotion'))
-            #sys.stdout = old_stdout # reset old stdout
-            maxStr=""
-            maxVal=0
-            
-            for key,val in result["emotion"].items():
-                if val>maxVal:
-                    maxStr=key
-                    maxVal=val
-            print(emotions[maxStr])
-            requests.post(url, data = {'emotion': emotions[maxStr]})
-        else:
-            cv2.imshow('screen', frame)
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
+    with  mss() as sct:
+        while True:
+            sct_img = sct.grab(bounding_box)
+            frame=np.array(sct_img)
+
+            if (isPosition != "true"):
+                #sys.stdout = open(os.devnull, "w")
+                try:
+                    result=DeepFace.analyze(img_path=frame,actions=['emotion'])
+                except:
+                    continue
+                #sys.stdout = old_stdout # reset old stdout
+                maxStr=""
+                maxVal=0
+                
+                for key,val in result["emotion"].items():
+                    if val>maxVal:
+                        maxStr=key
+                        maxVal=val
+                print(emotions[maxStr])
+                #requests.post(url, data = {'emotion': emotions[maxStr]})
+            else:
+                cv2.imshow('screen', frame)
+                if cv2.waitKey(1) & 0xFF == 27:
+                    break
 
     cv2.destroyAllWindows()
 
