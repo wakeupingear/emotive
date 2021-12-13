15231939293a9fcad48b185f7cdd968fe0241721
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-12-12 23:04:59 -0800

    face border setup for camera

--- ML/camera.py
+++ ML/camera.py
@@ -9,7 +9,7 @@ import logging
 import requests
 
 def main():
-    faceCascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faceCascade=cv2.CascadeClassifier('D:\Downloads\opencv-master\opencv-master\data\haarcascades\haarcascade_frontalface_default.xml')
 
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
@@ -26,6 +26,11 @@ def main():
 
         font=cv2.FONT_HERSHEY_SIMPLEX
 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rects = faceCascade.detectMultiScale(gray, 1.3, 2)
+        if len(rects) >= 0:
+            for (x, y, w, h) in rects:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         cv2.putText(frame,result['dominant_emotion'],(50,50),font,1,(0,0,255),2,cv2.LINE_4)
         cv2.imshow('Camera',frame)
 
